6dedd7428c891377549588314138dfb827035082
Niranjan Chaudhary <cdyniranjan7321@gmail.com>
2024-04-22 09:43:38 +0545

    change in About.jsx page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,16 +12,12 @@ const About = () => {
           Why Choose Us?
         </h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          architecto quisquam delectus minima perferendis nulla quia nisi
-          laborum, exercitationem cum quo accusantium, impedit sed. Dicta, quo
-          molestias omnis reprehenderit quae animi? Explicabo quasi accusamus
-          laboriosam temporibus delectus, aut a? Quasi?
+        No one wants minor preferences because of work, and there is no hindrance. 
+        It is said that every difficulty is in our minds. 
+        I will explain almost all of the hard work at times, or what? Almost?
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi,
-          suscipit reiciendis accusamus recusandae eum aspernatur pariatur odit
-          veritatis facere. Magnam!
+        Commissions, we reject accusations, and we refuse him from nature to make the truth. Great!"
         </p>
         <div className=" flex justify-center lg:justify-start">
           <Button title="Learn More" />
